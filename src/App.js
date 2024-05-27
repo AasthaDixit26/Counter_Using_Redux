e184a8cead7d9150a2ed460f8e2f3e17b10a957c
Aastha Dixit <aaasthadixit@gmail.com>
2024-05-27 13:05:47 +0530

    This is the counter application using React-Redux to get handson this

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Counter from './Components/Count';
+import { useDispatch } from 'react-redux';
 function App() {
+  const dispatch=useDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className='App'>
+      <button onClick={e=>dispatch({type:'INCREMENT'})}>Increment</button>
+      <Counter/>
+      <button onClick={e=>dispatch({type:'DECREMENT'})}>Decrement</button>
+   </div>
   );
 }
 
